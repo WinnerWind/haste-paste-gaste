5a892220fec0a7ba844db58f4edac5506a52a85d
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-08-01 21:12:04 +0530

    Basic pasting functionality

--- mainScript.js
+++ mainScript.js
@@ -13,7 +13,29 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-function handleFormData(inputValue, textareaValue) {
+function handleFormData(fileName, content) {
   // Do something with inputValue and textareaValue
-  alert(`Input: ${inputValue}\nTextarea: ${textareaValue}`);
+  alert(`Input: ${fileName}\nTextarea: ${content}`);
+  createNewPaste(fileName,content)
+}
+
+function createNewPaste(fileName,content) {
+  const url = "https://api.winnerwind.in/pastebin"
+  // const url = "http://192.168.68.105:5000/pastebin"
+  const headers = {
+    'content-type': 'application/json',
+  }
+  fetch(url, {method: 'POST', headers : headers , body: JSON.stringify(
+  {
+    "content": content,
+    "filename": fileName,
+    "is_link": false
+  })
+  }).then(response => response.json())
+  .then(data => {
+  console.log('Success:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
 }
